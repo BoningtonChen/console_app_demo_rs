6fd70c6ce5bec21935c81228e958b0ec80a95e75
Bonington <yikun.chen@163.com>
2024-07-11 18:05:46 +0800

    将应用逻辑转移至lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-
-        if args.len() < 3 {
-            return Err("NOT enough arguments!");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-// fn parse_config(args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let filename = args[2].clone();
-//
-//     Config { query, filename }
-// }
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -39,9 +14,10 @@ fn main() {
     println!("Searching for: {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong when reading the file!");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
 
-    println!("With text:\n{}",contents);
+        process::exit(1);
+    }
 
 }
